a5bb5f06998de40930c2edda83b0a853672e025d
Daniela Leguari <danielaleguari@outlook.com>
2022-10-19 08:00:05 -0300

    iniciado homework 2

---
+++ homework-2/js/script.js
@@ -0,0 +1,89 @@
+// Exercício 1
+
+function alterarTexto() {
+    const texto = document.getElementById('alterartexto');
+    texto.innerText = 'Texto alterado com sucesso!';
+}
+
+const btnAlteraTexto = document.querySelector('.altera-texto');
+btnAlteraTexto.addEventListener('click', () => {
+    alterarTexto();
+});
+
+
+// Exercício 2
+
+let campoCor = document.querySelector('#cor');
+campoCor.addEventListener('click', () => {
+    let corEscolhida = document.getElementById('corescolhida').value;
+    document.querySelector('#cor').style.color = corEscolhida;
+});
+
+
+//Exercício 3
+//adicionar + 1
+function adicionaMaisUm() {
+    let valor = parseInt(document.getElementById('adiciona').value);
+    valor += 1;
+    document.getElementById('adiciona').value = valor;
+}
+
+const btnAdicionaMaisUm = document.getElementById('adicionar');
+btnAdicionaMaisUm.addEventListener('click', () => {
+    adicionaMaisUm();
+})
+
+// resetar
+function resetaContador() {
+    let resetaContador = document.querySelector('#adiciona');
+    console.log(resetaContador)
+    resetaContador.value = 0;
+}
+
+//exercicio 4
+
+const mudaFiltro1 = () => {
+    let imagem = document.querySelector('#imagem');
+    imagem.src = "./img/photo-2.png";
+}
+
+const mudaFiltro2 = () => {
+    let imagem = document.querySelector('#imagem');
+    imagem.src = "./img/photo-3.png";
+}
+
+const resetaFiltro = () => {
+    let imagem = document.querySelector('#imagem');
+    imagem.src = "./img/photo-1.jpeg";
+}
+
+//exercicio 5
+
+const calcular = () => {
+    let valor1 = Number(document.querySelector('#valor1').value);
+    let valor2 = Number(document.querySelector('#valor2').value);
+    let operacao = document.querySelector('#operacao').value;
+    let resultado = 0;
+    if (operacao == 'somar') {
+        resultado = valor1 + valor2;
+    } else if (operacao == 'subtrair') {
+        resultado= valor1 - valor2;
+    } else if (operacao == 'multiplicar') {
+        resultado = valor1 * valor2;
+    } else {
+        resultado = valor1 / valor2;
+    }
+    document.querySelector('#resultado').value = resultado;
+
+}
+
+const limparCalculadora = () => {
+    document.querySelector('#valor1').value = '';
+    document.querySelector('#valor2').value= '';
+    document.querySelector('#resultado').value= '';
+}
+
+
+//exercicio 6
+
+
