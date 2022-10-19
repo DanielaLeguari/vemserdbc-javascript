065eb1f6808b8f98c423d19b9340fc2f6b681cde
Daniela Leguari <danielaleguari@outlook.com>
2022-10-19 19:15:03 -0300

    concluida atividade 1

--- homework-3/js/script.js
+++ homework-3/js/script.js
@@ -1,1 +1,30 @@
-let nota1 = prompt
+
+const calcularMedia = () => {
+    var nota1 = parseFloat(document.getElementById('nota1').value);
+    var nota2 = parseFloat(document.getElementById('nota2').value);
+    var nota3 = parseFloat(document.getElementById('nota3').value);
+    if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3)) {
+        document.getElementById('resultado').innerText = 'Você digitou valores inválidos!';
+        return;
+    }
+    if ((nota1 < 0 || nota2 < 0 || nota3 < 0) || (nota1 > 10 || nota2 > 10 || nota3 > 10)) {
+        document.getElementById('resultado').innerText = 'Você digitou valores inválidos!';
+        return;
+    }
+    let media = (nota1 + nota2 + nota3) / 3;
+    media = media.toFixed(2);
+    if (media < 5) {
+        document.getElementById('resultado').innerText = `A média é ${media}, você está REPROVADO!`;
+    } else if (media >= 5 && media < 7) {
+        document.getElementById('resultado').innerText = `A média é ${media}, você está em RECUPERAÇÃO!`;
+    } else {
+        document.getElementById('resultado').innerText = `A média é ${media}, PARABÉNS! Você está APROVADO!`;
+    }
+
+}
+
+
+
+
+
+
